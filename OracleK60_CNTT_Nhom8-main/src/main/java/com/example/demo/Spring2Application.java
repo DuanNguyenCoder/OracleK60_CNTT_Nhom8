bddf06c9197de9c9a8c5aae01856885922763361
BuiQuoclam <chimnon0212@gmail.com>
2022-10-29 22:03:56 +0700

    create view

---
+++ OracleK60_CNTT_Nhom8-main/src/main/java/com/example/demo/Spring2Application.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2Application implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2Application.class, args);
+	System.out.println("running");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("success");
+		
+	}
+
+}
